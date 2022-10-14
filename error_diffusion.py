790a616d86a7af3a65bba6978fdd26c2a6e0240f
mitk23 <mitk232@gmail.com>
2022-10-14 23:45:53 +0900

    black, flake8, isort & linted scripts

--- error_diffusion.py
+++ error_diffusion.py
@@ -1,42 +1,46 @@
 import numpy as np
+
 import constants
 
+
 def error_diffusion(img):
-  '''
-  halftoning an image by error diffusion
-  default filter: Jarvis-Judice-Ninke
+    """
+    halftoning an image by error diffusion
+    default filter: Jarvis-Judice-Ninke
 
-  Parameters
-  ----------
-  img: np.ndarray[np.ndarray, np.uint8]
-    grayscale image
-  
-  Returns
-  -------
-  img_diffused: np.ndarray[np.ndarray, np.uint8]
-    halftoned image
-  '''
-  err_filter = constants.FILTER_JARVIS_JUDICE_NINKE
+    Parameters
+    ----------
+    img: np.ndarray[np.ndarray, np.uint8]
+        grayscale image
 
-  H, W = img.shape[:2]
+    Returns
+    -------
+    img_diffused: np.ndarray[np.ndarray, np.uint8]
+        halftoned image
+    """
+    err_filter = constants.FILTER_JARVIS_JUDICE_NINKE
 
-  img_diffused = img.astype(np.float64)
+    H, W = img.shape[:2]
 
-  for row in range(H-2):
-    for col in range(2, W-2):
-      if img_diffused[row, col] < constants.THRESH:
-        err = img_diffused[row, col] - constants.BLACK
-        img_diffused[row, col] = constants.BLACK
-      else:
-        err = img_diffused[row, col] - constants.WHITE
-        img_diffused[row, col] = constants.WHITE
+    img_diffused = img.astype(np.float64)
 
-      img_diffused[row:row+3, col-2:col+3] += err * err_filter
-      
-  img_diffused = np.clip(img_diffused, constants.BLACK, constants.WHITE).astype(np.uint8)
+    for row in range(H - 2):
+        for col in range(2, W - 2):
+            if img_diffused[row, col] < constants.THRESH:
+                err = img_diffused[row, col] - constants.BLACK
+                img_diffused[row, col] = constants.BLACK
+            else:
+                err = img_diffused[row, col] - constants.WHITE
+                img_diffused[row, col] = constants.WHITE
 
-  return img_diffused
+            img_diffused[row : row + 3, col - 2 : col + 3] += err * err_filter
+
+    img_diffused = np.clip(img_diffused, constants.BLACK, constants.WHITE).astype(
+        np.uint8
+    )
+
+    return img_diffused
 
 
 def parallel_error_diffusion(img0, img1, img2):
-  pass
+    pass
--- main.py
+++ main.py
@@ -1,29 +1,31 @@
 import cv2
-import vss
+
 import util
+import vss
+
 
 def main():
-  img_sheet0, img_sheet1, img_secret = util.read_3images()
+    img_sheet0, img_sheet1, img_secret = util.read_3images()
 
-  # grayscale
-  img_sheet0 = cv2.cvtColor(img_sheet0, cv2.COLOR_BGR2GRAY)
-  img_sheet1 = cv2.cvtColor(img_sheet1, cv2.COLOR_BGR2GRAY)
-  img_secret = cv2.cvtColor(img_secret, cv2.COLOR_BGR2GRAY)
+    # grayscale
+    img_sheet0 = cv2.cvtColor(img_sheet0, cv2.COLOR_BGR2GRAY)
+    img_sheet1 = cv2.cvtColor(img_sheet1, cv2.COLOR_BGR2GRAY)
+    img_secret = cv2.cvtColor(img_secret, cv2.COLOR_BGR2GRAY)
 
-  # encrypt
-  sheet0, sheet1 = vss.evss(img_sheet0, img_sheet1, img_secret)
-  decoded = vss.decode(sheet0, sheet1)
+    # encrypt
+    sheet0, sheet1 = vss.evss(img_sheet0, img_sheet1, img_secret)
+    decoded = vss.decode(sheet0, sheet1)
 
-  # show
-  cv2.imshow('Sheet0', sheet0)
-  cv2.imshow('Sheet1', sheet1)
-  cv2.imshow('Decoded', decoded)
+    # show
+    cv2.imshow("Sheet0", sheet0)
+    cv2.imshow("Sheet1", sheet1)
+    cv2.imshow("Decoded", decoded)
 
-  print('press any key to terminate viewing')
+    print("press any key to terminate viewing")
 
-  cv2.waitKey(0)
-  cv2.destroyAllWindows()
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
 
-if __name__ == '__main__':
-  main()
+if __name__ == "__main__":
+    main()
